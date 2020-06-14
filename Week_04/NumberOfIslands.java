411bcc37843e276fc10aa58a553d8634c60a7cc8
Mason <lixq@weilaicheng.com>
2020-06-14 23:17:33 +0800

    第4周作业

---
+++ Week_04/NumberOfIslands.java
@@ -0,0 +1,82 @@
+// DFS
+class Solution {
+    int r, c;
+
+    public int numIslands(char[][] grid) {
+        int count = 0;
+
+        r = grid.length;
+        if (r <= 0) return 0;
+        c = grid[0].length;
+
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                if (grid[i][j] == '1') {
+                    dfs(grid, i, j);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    private void dfs(char[][] grid, int i, int j) {
+        if (i < 0 || j < 0 || i >= r || j >= c || grid[i][j] != '1') {
+            return;
+        }
+        grid[i][j] = '0';
+        dfs(grid, i + 1, j);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
+    }
+}
+
+
+
+// BFS
+class Solution {
+    public int numIslands(char[][] grid) {
+
+        if (grid == null || grid.length == 0) return 0;
+
+        int nr = grid.length;
+        int nc = grid[0].length;
+        int num_islands = 0;
+
+        for (int r = 0; r < nr; r++) {
+            for (int c = 0; c < nc; c++) {
+                if (grid[r][c] == '1') {
+                    num_islands++;
+                    grid[r][c] = '0';
+                    Queue<Integer> neighbors = new LinkedList<>();
+                    neighbors.add(r * nc + c);
+                    while (!neighbors.isEmpty()) {
+                        int id = neighbors.remove();
+                        int row = id / nc;
+                        int col = id % nc;
+                        if (row - 1 >= 0 && grid[row - 1][col] == '1') {
+                            neighbors.add((row - 1) * nc + col);
+                            grid[row - 1][col] = '0';
+                        }
+                        if (row + 1 < nr && grid[row + 1][col] == '1') {
+                            neighbors.add((row + 1) * nc + col);
+                            grid[row + 1][col] = '0';
+                        }
+                        if (col - 1 >= 0 && grid[row][col - 1] == '1') {
+                            neighbors.add(row * nc + col - 1);
+                            grid[row][col - 1] = '0';
+                        }
+                        if (col + 1 < nc && grid[row][col + 1] == '1') {
+                            neighbors.add(row * nc + col + 1);
+                            grid[row][col + 1] = '0';
+                        }
+                    }
+                }
+
+            }
+        }
+        return num_islands;
+
+    }
+}
--- Week_04/README.md
+++ Week_04/README.md
@@ -1,1 +1,86 @@
-学习笔记
+# 学习笔记
+
+### 本周学习内容：BFS， DFS，贪心算法，二分查找
+
+DFS代码模板
+1、递归写法
+
+```python
+visited = set()
+def dfs(node, visited):
+    if node in visited: # terminator
+    # already visited 
+    return
+    
+    visited.add(node)
+
+    # process current node here
+    for next_node in node.children():
+        if next_node not in visited:
+            dfs(next_node, visited)
+
+```
+
+2、非递归写法
+```python
+def DFS(self, tree):
+    if tree.root id None:
+        return []
+
+    visited, stack = [], [tree.root]
+
+    while stack:
+        node = stack.pop()
+        visited.add(node)
+
+        process(node)
+        nodes = generate_ralated_nodes(node)
+        stack.push(nodes)
+
+    # other processing work
+
+```
+
+BFS代码模板
+
+```python
+
+def BFS(graph, start, end):
+    visited = set()
+    queue = []
+    queue.append([start])
+    
+    while queue:
+    node = queue.pop()
+    visited.add(node)
+
+    process(node)
+    nodes = generate_ralated_nodes(node)
+    queue.push(nodes)
+
+    # other processing work
+
+```
+        
+
+
+
+使用二分查找，需要满足三个条件：函数单调性、有边界、能够通过索引访问
+二分查找代码模板
+#Python
+```python
+
+left, right = 0, len(array) - 1
+    while left <= right:
+    mid = (left + right) / 2
+    if array[mid] == target:
+        # find the target
+        break or return result
+
+    elif array[mid] < target:
+        left = mid + 1
+    else:
+        right = mid -1
+
+```
+    
